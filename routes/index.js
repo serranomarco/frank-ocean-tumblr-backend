e4b5be547e4225b1668c820c09395ef24ca5c140
serranomarco <serranomarco@hotmail.com>
2021-09-02 18:24:02 -0700

    wip

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({ message: 'welcome to endless' })
+    res.json({ message: 'welcome to Endless' })
 });
 
 module.exports = router;
