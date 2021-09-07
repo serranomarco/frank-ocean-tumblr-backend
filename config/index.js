9310c4821d187fe9aa1aebb5459f45fb0abfebb1
serranomarco <serranomarco@hotmail.com>
2021-09-07 11:00:59 -0700

    ssl

--- config/index.js
+++ config/index.js
@@ -5,8 +5,7 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST,
-        ssl: true
+        host: process.env.DB_HOST
     },
     jwtConfig: {
         secret: process.env.JWT_SECRET,
